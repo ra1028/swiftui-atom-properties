e863d6165edff56b3006a62f716b50017ad55d1c
ra1028 <r.fe51028.r@gmail.com>
2023-05-06 16:25:46 +0900

    Refactoring

--- Examples/Packages/iOS/Sources/ExampleVoiceMemo/VoiceMemoListScreen.swift
+++ Examples/Packages/iOS/Sources/ExampleVoiceMemo/VoiceMemoListScreen.swift
@@ -11,9 +11,6 @@ struct VoiceMemoListScreen: View {
     @Watch(AudioRecordPermissionAtom())
     var audioRecorderPermission
 
-    @Watch(RecordingElapsedTimeAtom())
-    var elapsedTimePhase
-
     @Watch(IsRecordingAtom())
     var isRecording
 
@@ -29,70 +26,100 @@ struct VoiceMemoListScreen: View {
                 ForEach($voiceMemos, id: \.url) { $voiceMemo in
                     VoiceMemoRow(voiceMemo: $voiceMemo)
                 }
-                .onDelete { IndexSet in
-                    actions.delete(at: IndexSet)
+                .onDelete { indexSet in
+                    actions.delete(at: indexSet)
                 }
             }
 
             VStack {
-                ZStack {
-                    switch audioRecorderPermission {
-                    case .undetermined, .granted:
-                        Circle()
-                            .foregroundColor(Color(.label))
-                            .frame(width: 74, height: 74)
+                switch audioRecorderPermission {
+                case .undetermined, .granted:
+                    RecordingButton()
 
-                        Button {
-                            withAnimation(.spring()) {
-                                actions.toggleRecording()
-                            }
-                        } label: {
-                            RoundedRectangle(cornerRadius: isRecording ? 4 : 35)
-                                .foregroundColor(Color(.systemRed))
-                                .padding(isRecording ? 18 : 2)
-                        }
-                        .frame(width: 70, height: 70)
+                case .denied:
+                    PermissionDeniedContent()
 
-                    case .denied:
-                        VStack(spacing: 10) {
-                            Text("Recording requires microphone access.")
-                                .multilineTextAlignment(.center)
-
-                            Button("Open Settings") {
-                                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
-                            }
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 74)
-
-                    @unknown default:
-                        EmptyView()
-                    }
+                @unknown default:
+                    PermissionDeniedContent()
                 }
 
-                let elapsedTime = elapsedTimePhase.value ?? .zero
-
-                if isRecording, let formattedDuration = dateComponentsFormatter.string(from: elapsedTime) {
-                    Text(formattedDuration)
-                        .font(.body.monospacedDigit().bold())
-                        .foregroundColor(.white)
-                        .colorMultiply(Color(Int(elapsedTime).isMultiple(of: 2) ? .systemRed : .label))
-                        .animation(.easeInOut(duration: 0.5), value: elapsedTime)
+                if isRecording {
+                    ElapsedTimeDisplay()
                 }
             }
             .padding()
-            .alert(
-                "Voice memo recording failed.",
-                isPresented: $isRecordingFailed,
-                actions: {}
-            )
-            .alert(
-                "Voice memo playback failed.",
-                isPresented: $isPlaybackFailed,
-                actions: {}
-            )
         }
         .navigationTitle("Voice Memo")
         .background(Color(.tertiarySystemBackground))
+        .ignoresSafeArea(.keyboard)
+        .alert(
+            "Voice memo recording failed.",
+            isPresented: $isRecordingFailed,
+            actions: {}
+        )
+        .alert(
+            "Voice memo playback failed.",
+            isPresented: $isPlaybackFailed,
+            actions: {}
+        )
+    }
+}
+
+private struct RecordingButton: View {
+    @Watch(IsRecordingAtom())
+    var isRecording
+
+    @Watch(VoiceMemoActionsAtom())
+    var actions
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(Color(.label))
+                .frame(width: 74, height: 74)
+
+            Button {
+                withAnimation(.spring()) {
+                    actions.toggleRecording()
+                }
+            } label: {
+                RoundedRectangle(cornerRadius: isRecording ? 4 : 35)
+                    .foregroundColor(Color(.systemRed))
+                    .padding(isRecording ? 18 : 2)
+            }
+            .frame(width: 70, height: 70)
+        }
+    }
+}
+
+private struct PermissionDeniedContent: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Text("Recording requires microphone access.")
+                .multilineTextAlignment(.center)
+
+            Button("Open Settings") {
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: 74)
+    }
+}
+
+private struct ElapsedTimeDisplay: View {
+    @Watch(RecordingElapsedTimeAtom())
+    var elapsedTimePhase
+
+    var body: some View {
+        let elapsedTime = elapsedTimePhase.value ?? .zero
+
+        if let formattedDuration = dateComponentsFormatter.string(from: elapsedTime) {
+            Text(formattedDuration)
+                .font(.body.monospacedDigit().bold())
+                .foregroundColor(.white)
+                .colorMultiply(Color(Int(elapsedTime).isMultiple(of: 2) ? .systemRed : .label))
+                .animation(.easeInOut(duration: 0.5), value: elapsedTime)
+        }
     }
 }
 
