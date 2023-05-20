e2f1076df43a3f667a610accb9ec45b6e0cb42a4
Ryo Aoyama <r.fe51028.r@gmail.com>
2023-05-20 18:33:43 +0900

    Use #fileID instead of #file (#70)

--- Sources/Atoms/Core/SourceLocation.swift
+++ Sources/Atoms/Core/SourceLocation.swift
@@ -1,4 +1,9 @@
 internal struct SourceLocation: Equatable {
     let fileID: String
     let line: UInt
+
+    init(fileID: String = #fileID, line: UInt = #line) {
+        self.fileID = fileID
+        self.line = line
+    }
 }
