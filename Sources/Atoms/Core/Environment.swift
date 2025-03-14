f133877a1c18fb6e443a25aea6eac426da3bf902
ra1028 <r.fe51028.r@gmail.com>
2025-03-14 15:51:59 +0900

    Use @Entry

--- Sources/Atoms/Core/Environment.swift
+++ Sources/Atoms/Core/Environment.swift
@@ -1,14 +1,21 @@
 import SwiftUI
 
-internal extension EnvironmentValues {
-    var store: StoreContext? {
-        get { self[StoreEnvironmentKey.self] }
-        set { self[StoreEnvironmentKey.self] = newValue }
+#if compiler(>=6)
+    internal extension EnvironmentValues {
+        @Entry
+        var store: StoreContext? = nil
     }
-}
+#else
+    internal extension EnvironmentValues {
+        var store: StoreContext? {
+            get { self[StoreEnvironmentKey.self] }
+            set { self[StoreEnvironmentKey.self] = newValue }
+        }
+    }
 
-private struct StoreEnvironmentKey: EnvironmentKey {
-    static var defaultValue: StoreContext? {
-        nil
+    private struct StoreEnvironmentKey: EnvironmentKey {
+        static var defaultValue: StoreContext? {
+            nil
+        }
     }
-}
+#endif
