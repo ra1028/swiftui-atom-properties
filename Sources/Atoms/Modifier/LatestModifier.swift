bc282380abeea63e385baa8dc51afd5cdc7e835c
ra1028 <ryo-aoyama@mercari.com>
2025-10-03 17:37:09 +0900

    Implement latest modifier

---
+++ Sources/Atoms/Modifier/LatestModifier.swift
@@ -0,0 +1,125 @@
+public extension Atom {
+    /// Provides the latest value where the specified key path returns `true` instead of the current value.
+    ///
+    /// ```swift
+    /// struct Item {
+    ///     let id: Int
+    ///     let isValid: Bool
+    /// }
+    ///
+    /// struct ItemAtom: StateAtom, Hashable {
+    ///     func defaultValue(context: Context) -> Item {
+    ///         Item(id: 0, isValid: false)
+    ///     }
+    /// }
+    ///
+    /// struct ExampleView: View {
+    ///     @Watch(ItemAtom())
+    ///     var currentItem
+    ///
+    ///     @Watch(ItemAtom().latest(\.isValid))
+    ///     var latestValidItem
+    ///
+    ///     var body: some View {
+    ///         VStack {
+    ///             Text("Current ID: \(currentItem.id)")
+    ///             Text("Latest Valid ID: \(latestValidItem?.id ?? 0)")
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    #if hasFeature(InferSendableFromCaptures)
+        func latest(_ keyPath: any KeyPath<Produced, Bool> & Sendable) -> ModifiedAtom<Self, LatestModifier<Produced>> {
+            modifier(LatestModifier(keyPath: keyPath))
+        }
+    #else
+        func latest(_ keyPath: KeyPath<Produced, Bool>) -> ModifiedAtom<Self, LatestModifier<Produced>> {
+            modifier(LatestModifier(keyPath: keyPath))
+        }
+    #endif
+}
+
+/// A modifier that provides the latest value where the specified key path returns `true` instead of the current value.
+///
+/// Use ``Atom/latest(_:)`` instead of using this modifier directly.
+public struct LatestModifier<Base>: AtomModifier {
+    /// A type of base value to be modified.
+    public typealias Base = Base
+
+    /// A type of value the modified atom produces.
+    public typealias Produced = Base?
+
+    #if hasFeature(InferSendableFromCaptures)
+        /// A type representing the stable identity of this modifier.
+        public struct Key: Hashable, Sendable {
+            private let keyPath: any KeyPath<Base, Bool> & Sendable
+
+            fileprivate init(keyPath: any KeyPath<Base, Bool> & Sendable) {
+                self.keyPath = keyPath
+            }
+        }
+
+        private let keyPath: any KeyPath<Base, Bool> & Sendable
+
+        internal init(keyPath: any KeyPath<Base, Bool> & Sendable) {
+            self.keyPath = keyPath
+        }
+
+        /// A unique value used to identify the modifier internally.
+        public var key: Key {
+            Key(keyPath: keyPath)
+        }
+    #else
+        public struct Key: Hashable, Sendable {
+            private let keyPath: UnsafeUncheckedSendable<KeyPath<Base, Bool>>
+
+            fileprivate init(keyPath: UnsafeUncheckedSendable<KeyPath<Base, Bool>>) {
+                self.keyPath = keyPath
+            }
+        }
+
+        private let _keyPath: UnsafeUncheckedSendable<KeyPath<Base, Bool>>
+        private var keyPath: KeyPath<Base, Bool> {
+            _keyPath.value
+        }
+
+        internal init(keyPath: KeyPath<Base, Bool>) {
+            _keyPath = UnsafeUncheckedSendable(keyPath)
+        }
+
+        /// A unique value used to identify the modifier internally.
+        public var key: Key {
+            Key(keyPath: _keyPath)
+        }
+    #endif
+
+    /// A producer that produces the value of this atom.
+    public func producer(atom: some Atom<Base>) -> AtomProducer<Produced> {
+        AtomProducer { context in
+            context.transaction { context in
+                let value = context.watch(atom)
+                let storage = context.watch(StorageAtom())
+
+                if value[keyPath: keyPath] {
+                    storage.latest = value
+                }
+
+                return storage.latest
+            }
+        }
+    }
+}
+
+private extension LatestModifier {
+    @MainActor
+    final class Storage {
+        var latest: Base?
+    }
+
+    struct StorageAtom: ValueAtom, Hashable {
+        func value(context: Context) -> Storage {
+            Storage()
+        }
+    }
+}
